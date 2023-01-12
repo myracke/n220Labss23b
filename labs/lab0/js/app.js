167ef52c9671976ee2854d605ec36046182f249c
Myranda Estes <mestes2012@outlook.com>
2023-01-12 15:55:50 -0500

    makingbase

---
+++ labs/lab0/js/app.js
@@ -0,0 +1,14 @@
+function setup() {
+
+    createCanvas(800,600);
+
+
+
+    FileList(76,19,102);
+    rect(40,50,100,20);
+    backgroud(227,50,100,20);
+
+}
+function draw (){
+    circle(mouseX, mouseY, 17);
+}
